f3f4bbdd873b8bbd636ccf7b421705c0eafd4343
fort1k <akremhamdi29@gmail.com>
2025-12-05 02:48:05 +0100

    Testing commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return(
+        <div>i am a testing footer</div>
+    )
+}
---
+++ src/components/Selection.jsx
@@ -0,0 +1,5 @@
+export default function Selection(){
+    return(
+        <div>i am a testing selection</div>
+    )
+}
