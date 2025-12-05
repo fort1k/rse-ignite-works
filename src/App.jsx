d7a7a916ea95e1aa68dbe2f4b7247c11dd97dce9
fort1k <akremhamdi29@gmail.com>
2025-12-05 07:19:17 +0100

    Patched again

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Footer from "./components/Footer.jsx"
 import Signin from "./components/Signin.jsx"
 import Evaluation from "./components/evaluation.jsx"
-import Resultat from "./components/Resultat.jsx"
 
 export default function App() {
   return(
